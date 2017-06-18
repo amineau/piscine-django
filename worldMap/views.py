7182abb6976fc53282843e1db14bac7702e655ed
Antoine MINEAU <amineau@student.42.fr>
2017-06-18 15:07:59 +0200

    import DataClass

--- worldMap/views.py
+++ worldMap/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.conf import settings
+from r00.Data import Data
 
 # Create your views here.
 def index(request):
+    data = Data().load()
     move = request.GET.get('move', '')
+    new = request.GET.get('new', '')
+    if new == 'true':
+        data.load_default_settings()
+    # print(data)
     context = {
-        'lines': range(settings.GRID[0]),
-        'columns': range(settings.GRID[1])
+        'lines': range(data['grid'][0]),
+        'columns': range(data['grid'][1])
     }
     return render(request, 'worldMap/index.html', context)
