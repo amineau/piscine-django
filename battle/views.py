c33a25a040a0ccbf9e472d7a31fca3aa7c45449a
Amine DERRAGUI <aderragu@e2r5p9.42.fr>
2017-06-18 18:11:47 +0200

    battle wip

--- battle/views.py
+++ battle/views.py
@@ -4,5 +4,5 @@ from r00.Data import Data
 # Create your views here.
 def index(request, moviemon_id):
     data = Data().load()
-    print(data.get_movie_by_id(int(moviemon_id)))
-    return render(request, 'battle/index.html', {'moviemon_id': moviemon_id})
+    movie=data.get_movie_by_id(int(moviemon_id))
+    return render(request, 'battle/index.html', {'Movie': movie})
