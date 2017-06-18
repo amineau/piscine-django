3e13d385bad37ef975d6e1df1befed082cb50c5e
Antoine MINEAU <amineau@student.42.fr>
2017-06-18 14:06:48 +0200

    worldmap initialize grid, buttons

--- worldMap/views.py
+++ worldMap/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def index(request):
-    return render(request, 'worldMap/index.html')
+    move = request.GET.get('move', '')
+    context = {
+        'lines': range(settings.GRID[0]),
+        'columns': range(settings.GRID[1])
+    }
+    return render(request, 'worldMap/index.html', context)
