4f1231ab4f2890fbc642c510fdec1fe5131ac23d
Amine DERRAGUI <aderragu@e2r5p9.42.fr>
2017-06-18 19:15:39 +0200

    battle wip

--- battle/views.py
+++ battle/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from r00.Data import Data
+import random
 
 # Create your views here.
 def index(request, moviemon_id):
     data = Data().load()
     movie=data.get_movie_by_id(int(moviemon_id))
-    return render(request, 'battle/index.html', {'Movie': movie})
+    return render(request, 'battle/index.html', {'Movie': movie,'strenght':data.get_strength(),'balls':data.get_movieBalls_count(),'rate':process_success_rate(data, movie)})
+
+def process_success_rate(data, movie):
+    bo = movie['box_office'] / 2
+    while(bo > 10):
+        bo = bo / 10
+    rate = 50 - bo * 10 + data.get_strength() * 5
+    print(rate)
+    rate = max(1, min(rate, 90))
+    return rate
