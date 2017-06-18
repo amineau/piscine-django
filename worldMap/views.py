4bb2257755b8d8142aa75048230e12db6d5d6792
Antoine MINEAU <amineau@student.42.fr>
2017-06-18 15:42:55 +0200

    worldmap add player and change settings.GRID and PLAYER_BEGIN list to dict

--- worldMap/views.py
+++ worldMap/views.py
@@ -4,14 +4,19 @@ from r00.Data import Data
 
 # Create your views here.
 def index(request):
-    data = Data().load()
     move = request.GET.get('move', '')
     new = request.GET.get('new', '')
+    data = Data().load()
     if new == 'true':
         data.load_default_settings()
-    # print(data)
+    settings = data.dump()
     context = {
-        'lines': range(data['grid'][0]),
-        'columns': range(data['grid'][1])
+        'grid': {
+            'x': range(settings['grid']['x']),
+            'y': range(settings['grid']['y']),
+        },
+        'player_position': settings['player_position'],
+        'movieballs': settings['player_movie_balls_count'],
     }
+    print()
     return render(request, 'worldMap/index.html', context)
