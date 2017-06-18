6324bc4a3f3f0a8c1467e7df7a26c4ffea44e401
anouvel <anouvel@student.42.fr>
2017-06-18 13:03:11 +0200

    find the good place to load settings

--- titleScreen/views.py
+++ titleScreen/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from r00.Data import Data
 
+
 # Create your views here.
 
 def index(request):
-    # change the position of this line to be started at server startup
-    d = Data().load_default_settings()
-    print(d)
-    # we need to use d = Data().load() instead of
+    data = Data().load()
+    print(data)
     return render(request, 'titleScreen/index.html')
