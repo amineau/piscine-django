ae1dfc070c60ca7378f6de7d59fef117277c94a7
Antoine MINEAU <amineau@student.42.fr>
2017-06-18 16:58:30 +0200

    option and save views

--- options/views.py
+++ options/views.py
@@ -5,7 +5,13 @@ def index(request):
     return render(request, 'options/index.html')
 
 def save_game(request):
-    return render(request, 'options/save_game.html')
+    slot = int(request.GET.get('slot', 0))
+    context = {
+        'slot_current': slot,
+        'slot_up': (slot - 1) % 3,
+        'slot_down': (slot + 1) % 3,
+    }
+    return render(request, 'options/save_game.html', context)
 
 def load_game(request):
     return render(request, 'options/load_game.html')
