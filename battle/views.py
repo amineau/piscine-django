465e80c6e1a8c4eaaee347540a4f597603797e06
Amine DERRAGUI <aderragu@e2r5p9.42.fr>
2017-06-18 16:50:02 +0200

    battle wip

--- battle/views.py
+++ battle/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from r00.Data import Data
 
 # Create your views here.
 def index(request, moviemon_id):
+    data = Data().load()
+    print(data)
     return render(request, 'battle/index.html', {'moviemon_id': moviemon_id})
